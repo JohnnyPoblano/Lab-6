b3e956af7f9bc19f240290498a4c4b1114dd211b
Johnny <johncgumm@gmail.com>
2018-10-19 11:23:01 -0700

    Finished fields and no-arg constructor

--- ClassSection.java
+++ ClassSection.java
@@ -3,3 +3,33 @@
 **  CIS 131
 **  Lab 6
 */
+
+public class ClassSection {
+
+    // Fields
+    private int crn;
+    private String departmentCode;
+    private int courseNumber;
+    private String instructionalMode;
+    private String campus;
+    private String meetingDays;
+    private String meetingTimes;
+    private int capacity;
+    private int enrollment;
+    private int instructorID;
+
+    // No-arg constructor
+    ClassSection() {
+        crn = 0;
+        departmentCode = "";
+        courseNumber = 0;
+        instructionalMode = "";
+        campus = "";
+        meetingDays = "";
+        meetingTimes = "";
+        capacity = 0;
+        enrollment = 0;
+        instructorID = 0;
+    }
+
+}
