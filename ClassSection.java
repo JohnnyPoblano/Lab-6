6a902dcd7309c3bfd5405caf5e43ac33f8659cfa
Johnny <johncgumm@gmail.com>
2018-10-28 20:52:25 -0700

    Added getters, setters, toString output override method

--- ClassSection.java
+++ ClassSection.java
@@ -55,4 +55,104 @@ public class ClassSection {
         instructorID = insID;
     }
 
+    // Mutator 'setter' methods
+    public void setCrn(int c) {
+        this.crn = c;
+    }
+
+    public void setDepartmentCode(String d) {
+        this.departmentCode = d;
+    }
+
+    public void setCourseNumber(int c) {
+        this.courseNumber = c;
+    }
+
+    public void setInstructionalMode(String i) {
+        this.instructionalMode = i;
+    }
+
+    public void setCampus(String c) {
+        this.campus = c;
+    }
+    
+    public void setMeetingDays(String m) {
+        this.meetingDays = m;
+    }
+    
+    public void setMeetingTimes(String m) {
+        this.meetingTimes = m;
+    }
+    
+    public void setCapacity(int c) {
+        this.capacity = c;
+    }
+
+    public void setEnrollment(int e) {
+        this.enrollment = e;
+    }
+
+    public void setInstructorID(int i) {
+        this.instructorID = i;
+    }
+
+    // Return 'getter' methods
+    public int getCrn() {
+        return crn;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public String getInstructionalMode() {
+        return instructionalMode;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+    
+    public String getMeetingDays() {
+        return meetingDays;
+    }
+    
+    public String getMeetingTimes() {
+        return meetingTimes;
+    }
+    
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getEnrollment() {
+        return enrollment;
+    }
+
+    public int getInstructorID() {
+        return instructorID;
+    }
+
+    // To string displays all fields
+    public String toString() {
+        String str = "************************************\n" +
+                     "CRN:                     " + crn + "\n" +
+                     "Department:              " + departmentCode + "\n" +
+                     "Course Number:           " + courseNumber + "\n" +
+                     "Instructional Mode:      " + instructionalMode + "\n" +
+                     "Campus:                  " + campus + "\n" +
+                     "Meeting Days:            " + meetingDays + "\n" +
+                     "Meeting Times:           " + meetingTimes + "\n" +
+                     "Capacity:                " + capacity  + "\n" +
+                     "Enrollment:              " + enrollment + "\n" +
+                     "Instructor's ID:         " + instructorID + "\n" +
+                     "************************************\n";
+
+        return str;
+    }
+
 }
